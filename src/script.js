52b655b6d0e9570615080cf54fd6ebb50f2fa4c1
Wojtek Janoszek <wetcouch46wojtek@gmail.com>
2015-05-14 22:57:42 +0200

    Fixes

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $(".desc-header").hide();
     $(".desc-text").hide();
 
-    function textPage (clickLink, showElement, showHeader, image, title) {
+    function textPage (clickLink, showElement, showHeader, image, title, isWhite) {
         header = ".desc-header";
         $(clickLink).click(function () {
             $(header).hide();
@@ -19,21 +19,39 @@ $(document).ready(function () {
             $(showElement).show();
             if (showHeader == true) {
                 $(header).show();
-                $(".desc-triangle").attr('src', image)
+                $(".desc-triangle").attr('src', image);
                 $(".desc-title").text(title)
             }
+            if (isWhite == true) {
+                $('.header-title').addClass('is-white')
+            } else {
+                $('.header-title').removeClass('is-white')
+            }
+
         })
     }
 
+    function openImage (link, page) {
+        $(link).click(function () {
+            window.open(page)
+        })
+    }
+
+    openImage('.img1', 'http://i.imgur.com/jSfx6DG.jpg');
+    openImage('.img2', 'http://i.imgur.com/hJmyqfT.jpg');
+    openImage('.img3', 'http://i.imgur.com/ukXlBEk.jpg');
+    openImage('.img4', 'http://i.imgur.com/r2KrLrW.jpg');
+    openImage('.img5', 'http://i.imgur.com/cdF2LYW.jpg');
+
     $(".link").click(function () {return false});
 
-    textPage ('.nav-project', '.desc-text-project', true, imageBlack, 'O Projekcie');
-    textPage ('.sport-cycling', '.desc-text-kolarstwo', true, imageGreen, 'Kolarstwo Górskie');
-    textPage('.sport-football', '.desc-text-football', true, imageGreen, 'Piłka Nożna');
-    textPage('.sport-weightlifting', '.desc-text-weightlifting', true, imageRed, 'Podnoszenie Ciężarów');
-    textPage('.sport-boks', '.desc-text-boks', true, imageBlue, 'Boks');
-    textPage('.sport-archery', '.desc-text-archery', true, imageRed, 'Łucznictwo');
-    textPage('.sport-gliding', '.desc-text-gliding', true, imageBlue, 'Szybownictwo Górskie');
-    textPage('.nav-school', '.desc-text-school', true, imageBlack, 'Sporty w szkole');
+    textPage ('.nav-project', '.desc-text-project', true, imageBlack, 'O Projekcie', true);
+    textPage ('.sport-cycling', '.desc-text-kolarstwo', true, imageGreen, 'Kolarstwo Górskie', false);
+    textPage('.sport-football', '.desc-text-football', true, imageGreen, 'Piłka Nożna', false);
+    textPage('.sport-weightlifting', '.desc-text-weightlifting', true, imageRed, 'Podnoszenie Ciężarów', false);
+    textPage('.sport-boks', '.desc-text-boks', true, imageBlue, 'Boks', false);
+    textPage('.sport-archery', '.desc-text-archery', true, imageRed, 'Łucznictwo', false);
+    textPage('.sport-gliding', '.desc-text-gliding', true, imageBlue, 'Szybownictwo Górskie', false);
+    textPage('.nav-school', '.desc-text-school', true, imageBlack, 'Sporty w szkole', true);
     textPage('.nav-history', '#content', false)
 });
